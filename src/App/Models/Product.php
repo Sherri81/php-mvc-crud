23e8da70b34b333841ffc126085635ce92863f81
Sherri81 <159568471+Sherri81@users.noreply.github.com>
2024-04-30 15:42:29 -0500

    init commit

---
+++ src/App/Models/Product.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+class Product
+{
+    public function getData(): array 
+    {
+        
+            $dsn = "mysql:
+            host=localhost;
+            dbname=adv_php;
+            charset=utf8;
+            port=3306";
+
+            $pdo = new PDO($dsn, "adv_php_user", "QnepmMdV", [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+
+            $stmt = $pdo->query("SELECT * FROM `products`");
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    // create an empty array to store errors
+    protected array $errors = [];
+
+    // method to add errors when validation fails
+    protected function addError(string $field, string $message): void
+    {
+        $this->errors[$field] = $message;
+    }
+
+    // method to test form data against validation requirements
+    protected function validate(array $data): bool
+    {
+        if (empty($data["name"])) {
+            
+            $this->addError("name", "Required");
+
+        }
+
+        return empty($this->errors);
+    }
+
+    // method for returning errors on validation
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+public function insert(array $data): int|bool
+        // validate form data
+        if ( ! $this->validate($data) ) {
+            return false;
+        }
+
+        $sql = "INSERT INTO `products` (name, description) VALUES (?, ?)";
+
+        $conn = $this->getConnection();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(1, $data["name"], PDO::PARAM_STR);
+        $stmt->bindValue(2, $data["description"], PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $conn->lastInsertId();
+    }
+    public function delete(string $id): bool
+    {
+        $sql = "DELETE FROM `products` WHERE id = :id";
+
+        $conn = $this->getConnection();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+}
